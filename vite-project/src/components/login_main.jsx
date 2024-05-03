205f030529fa187904f4b953126cb54b230757d0
hyeleeeeen <heo22969087@gmail.com>
2024-05-03 17:18:33 +0900

    로그인 화면 리팩토링

--- vite-project/src/components/login_main.jsx
+++ vite-project/src/components/login_main.jsx
@@ -1,67 +1,86 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const Main = () => {
-  const [isIdInput, setIsIdInput] = useState("");
-  const [isPwInput, setIsPwInput] = useState("");
-  const [isIdDelete, setIsIdDelete] = useState(false);
-  const [isPwDelete, setIsPwDelete] = useState(false);
+  const [loginInput, setLoginInput] = useState({ id: "", password: "" });
+  const [isInputDelete, setIsInputDelete] = useState(false);
   const [isLoginChecked, setIsLoginChecked] = useState(false);
   const [isIdFocus, setIsIdFocus] = useState(false);
   const [isPwFocus, setIsPwFocus] = useState(false);
-  const adImg = "https://ssl.pstatic.net/melona/libs/1378/1378592/b96ed7146a112e87d6ea_20240423164744254.png";
+  const adImg =
+    "https://ssl.pstatic.net/melona/libs/1378/1378592/b96ed7146a112e87d6ea_20240423164744254.png";
 
-  // useRef 사용해서 dom 선택한후 focus잡기
-
-  const refId = useRef(null);
-
-  const refPw = useRef(null);
+  localStorage.setItem("id1", "aass2296");
+  localStorage.setItem("pw1", "aass9087");
+  const localId = localStorage.getItem("id1");
+  const localPw = localStorage.getItem("pw1");
 
   const handleIdFocus = () => {
-    setIsIdFocus(!isIdFocus);
-    if (isPwFocus) setIsPwFocus(false);
+    setIsIdFocus(true);
+    setIsPwFocus(false);
     console.log("Id!");
   };
   const handlePwFocus = () => {
-    setIsPwFocus(!isPwFocus);
-    if (isIdFocus) setIsIdFocus(false);
+    setIsPwFocus(true);
+    setIsIdFocus(false);
     console.log("Pw!");
   };
 
-  const handleIdChange = (e) => {
-    setIsIdInput(e.target.value);
-    setIsIdDelete(e.target.value !== ""); // 입력값이 존재하면 true
-    console.log(isIdInput);
-  };
-
-  const handlePwChange = (e) => {
-    setIsPwInput(e.target.value);
-    setIsPwDelete(e.target.value !== ""); // 입력값이 존재하면 true
-    console.log(isPwInput);
+  const handleInputChange = (field, e) => {
+    setLoginInput((prev) => ({
+      ...prev,
+      [field]: e.target.value,
+    }));
+    console.log(loginInput);
+    setIsInputDelete((prev) => ({
+      ...prev,
+      [field]: e.target.value !== "",
+    }));
   };
 
   const handleIdDelete = () => {
-    if (refId.current) {
-      refId.current.value = ""; // 입력값을 공백으로 설정하여 삭제
-      setIsIdDelete(false);
-    }
+    setLoginInput((prev) => ({
+      ...prev,
+      id: "",
+    }));
+    setIsInputDelete((prev) => ({
+      ...prev,
+      id: false,
+    }));
+
     console.log("지워");
   };
 
   const handlePwDelete = () => {
-    if (refPw.current) {
-      refPw.current.value = ""; // 입력값을 공백으로 설정하여 삭제
-      setIsPwDelete(false);
-    }
+    setLoginInput((prev) => ({
+      ...prev,
+      password: "",
+    }));
+    setIsInputDelete((prev) => ({
+      ...prev,
+      password: false,
+    }));
+
+    console.log("지워");
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("로그인");
+    console.log(loginInput);
   };
 
   const handleCheck = () => {
     setIsLoginChecked(!isLoginChecked);
     console.log("checked!");
+  };
+
+  const isLoginRight = () => {
+    if (loginInput.id === "" || loginInput.password === "") {
+      alert("아이디와 비밀번호를 입력해주세요.");
+    } else if (localId === loginInput.id && localPw === loginInput.password) {
+      alert("로그인 성공");
+    } else {
+      alert("아이디 또는 비밀번호가 올바르지 않습니다.");
+    }
   };
 
   return (
@@ -76,18 +95,12 @@ const Main = () => {
               ID 로그인
             </span>
           </a>
-          <a
-            href="#"
-            className="flex-1 relative border pt-[17px] bg-[#f8f9fa] border-[#e3e3e3] border-r-0 border-b-0 z-[2] h-[61px]"
-          >
+          <a href="#" className="login-nav-a border-r-0">
             <span className="login-nav-btn left-[27px] before:bg-[left_calc(-208px)_top_calc(-234px)] ">
               일회용 번호
             </span>
           </a>
-          <a
-            href="#"
-            className="flex-1 relative border pt-[17px] bg-[#f8f9fa] border-[#e3e3e3] border-b-0 z-[2] h-[61px] rounded-tr-md"
-          >
+          <a href="#" className="login-nav-a rounded-tr-md">
             <span className="login-nav-btn left-[38px] before:bg-[left_calc(-244px)_top_calc(-234px)] ">
               QR코드
             </span>
@@ -97,10 +110,10 @@ const Main = () => {
           <form className="py-5 px-6 block" onSubmit={handleSubmit}>
             <div>
               <div
-                className={`relative text-left border ${isIdFocus ? "border-[#09aa5c]" : "border-[#dadada]"} rounded-tl-md rounded-tr-md w-full h-12 pt-[14px] pb-[13px] px-[17px]`}
+                className={`login-input-box ${isIdFocus ? "border-[#09aa5c]" : "border-[#dadada]"} rounded-tl-md rounded-tr-md `}
               >
                 <div
-                  className={`bg-login_img bg-[length:290px_275px] left-[18px] top-[15px] w-4 h-4 absolute ${isIdFocus ? "bg-[left_calc(-156px)_top_calc(-255px)]" : "bg-[left_calc(-175px)_top_calc(-255px)]"}`}
+                  className={`login-input-icon ${isIdFocus ? "bg-[left_calc(-156px)_top_calc(-255px)]" : "bg-[left_calc(-175px)_top_calc(-255px)]"}`}
                 >
                   <span className="blind">아이디</span>
                 </div>
@@ -109,23 +122,23 @@ const Main = () => {
                   placeholder="아이디"
                   title="아이디"
                   maxLength="41"
-                  ref={refId}
-                  onClick={handleIdFocus}
-                  onChange={handleIdChange}
-                  className="outline-none pr-[30px] ps-0.5 pe-0.5 left-[22px] relative w-[350px] text-[16px] text-[#222] z-[4] leading-[19px] cursor-pointer"
+                  onFocus={handleIdFocus}
+                  onChange={(e) => handleInputChange("id", e)}
+                  value={loginInput.id}
+                  className="login-input"
                 />
                 <span
-                  className={`absolute w-[18px] ${isIdDelete ? "inline-block" : "hidden"} h-[18px] top-[6px] right-2 z-[5] mt-2 ml-2 bg-login_img bg-[length:290px_275px] bg-[left_calc(-179px)_top_calc(-182px)]`}
+                  className={`login-input-delete ${isInputDelete.id ? "inline-block" : "hidden"} `}
                   onClick={handleIdDelete}
                 >
                   <span className="blind">삭제</span>
                 </span>
               </div>
               <div
-                className={`relative text-left border ${isPwFocus ? "border-[#09aa5c]" : "border-[#dadada]"} rounded-bl-md rounded-br-md w-full h-12 pt-[14px] pb-[13px] px-[17px]`}
+                className={`login-input-box ${isPwFocus ? "border-[#09aa5c]" : "border-[#dadada]"} rounded-bl-md rounded-br-md `}
               >
                 <div
-                  className={`bg-login_img bg-[length:290px_275px] left-[17px] top-[15px] w-4 h-4 absolute ${isPwFocus ? "bg-[left_calc(-210px)_top_calc(-255px)]" : "bg-[left_calc(-66px)_top_calc(-255px)]"}`}
+                  className={`login-input-icon ${isPwFocus ? "bg-[left_calc(-210px)_top_calc(-255px)]" : "bg-[left_calc(-66px)_top_calc(-255px)]"}`}
                 >
                   <span className="blind">비밀번호</span>
                 </div>
@@ -133,13 +146,13 @@ const Main = () => {
                   type="password"
                   placeholder="비밀번호"
                   title="비밀번호"
-                  onChange={handlePwChange}
-                  onClick={handlePwFocus}
-                  ref={refPw}
-                  className="outline-none pr-[30px] ps-0.5 pe-0.5 left-[22px] relative w-[350px] text-[16px] text-[#222] z-[4] leading-[19px] cursor-pointer"
+                  onChange={(e) => handleInputChange("password", e)}
+                  value={loginInput.password}
+                  onFocus={handlePwFocus}
+                  className="login-input"
                 />
                 <span
-                  className={`${isPwDelete ? "inline-block" : "hidden"} absolute w-[18px] h-[18px] top-[6px] right-2 z-[5] mt-2 ml-2 bg-login_img bg-[length:290px_275px] bg-[left_calc(-179px)_top_calc(-182px)]`}
+                  className={`login-input-delete ${isInputDelete.password ? "inline-block" : "hidden"}`}
                   onClick={handlePwDelete}
                 >
                   <span className="blind">삭제</span>
@@ -173,7 +186,7 @@ const Main = () => {
                   />
                   <label
                     htmlFor="switch"
-                    className="block relative w-[33px] h-[18px] rounded-xl bg-[#c3c8c9] my-0 ml-[5px]  before:block before:absolute before:top-1/2 before:left-[2px] before:w-[14px] before:h-[14px] before:mt-[-7px] before:rounded-full before:bg-white"
+                    className="block relative w-[33px] h-[18px] rounded-xl bg-[#c3c8c9] my-0 ml-[5px] before:block before:absolute before:top-1/2 before:left-[2px] before:w-[14px] before:h-[14px] before:mt-[-7px] before:rounded-full before:bg-white"
                   >
                     <span className="blind">off</span>
                   </label>
@@ -183,6 +196,7 @@ const Main = () => {
             <button
               type="submit"
               className="mt-[36px] w-full py-[13px] border border-[rgba(0,0,0,.15)] rounded-md bg-[#09aa5c]"
+              onClick={isLoginRight}
             >
               <span className="text-white font-bold leading-6 text-[20px]">
                 로그인
@@ -192,11 +206,19 @@ const Main = () => {
         </div>
       </section>
       <div className="leading-[17px] text-[14px] text-[#888] block pt-5 pb-[35px] tracking-[-.5px] m-0 px-0 w-full text-center">
-        <a href="#" className="inline-block">비밀번호 찾기</a>
-        <a href="#" className="inline-block relative pl-7 before:absolute before:w-px before:h-3 before:bg-[#dadada] before:top-[3px] before:left-3">아이디 찾기</a>
-        <a href="#" className="inline-block relative pl-7 before:absolute before:w-px before:h-3 before:bg-[#dadada] before:top-[3px] before:left-3">회원가입</a>
+        <a href="#" className="inline-block">
+          비밀번호 찾기
+        </a>
+        <a href="#" className="login-find">
+          아이디 찾기
+        </a>
+        <a href="#" className="login-find">
+          회원가입
+        </a>
       </div>
-      <aside className="w-[460px] block"><img src={adImg}></img></aside>
+      <aside className="w-[460px] block">
+        <img src={adImg}></img>
+      </aside>
     </>
   );
 };
